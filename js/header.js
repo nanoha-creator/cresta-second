7288474a2703b03aa07f1b861f21ccec4697ef62
nanoha <nanoha.creator@gmail.com>
2022-11-05 08:06:11 +0000

    update hamburgerBtn

--- js/header.js
+++ js/header.js
@@ -8,19 +8,3 @@ $(window).scroll(function () {
     }
   }
 });
-
-/* トグルボタン制御 */
-$(".bl_toggleBtn").click(function () {
-  $(this).toggleClass("is_close");
-  $(".bl_headerNav").toggleClass("is_fade");
-  $("body").toggleClass("is_noscroll");
-});
-
-/* メニューのリンクが押下されたときに、ナビゲーションを隠す */
-$(".bl_headerNav_link").click(function () {
-  if ($(window).width() < 768) {
-    $(".bl_headerNav").removeClass("is_fade");
-    $(".bl_toggleBtn").removeClass("is_close");
-    $("body").removeClass("is_noscroll");
-  }
-});
